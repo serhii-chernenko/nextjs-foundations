48c38b3b808d8f5118a8fc7972cc9afa54c7ea67
Serhii Chernenko <serhii.chernenko@vaimo.com>
2026-03-17 12:55:48 +0100

    fix(monorepo): next.config.ts fixed to support dynamic slug for apps/web

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,16 +5,25 @@ const blogUrl = process.env.BLOG_URL || 'http://localhost:3001';
 const nextConfig: NextConfig = {
   // biome-ignore lint/suspicious/useAwait: No async operations needed here, but Next.js expects a promise
   async rewrites() {
-    return [
-      {
-        source: '/blog',
-        destination: `${blogUrl}`,
-      },
-      {
-        source: '/blog/:path*',
-        destination: `${blogUrl}/:path*`,
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: '/blog',
+          destination: `${blogUrl}`,
+        },
+        {
+          source: '/blog/:path*',
+          destination: `${blogUrl}/:path*`,
+        },
+      ],
+      afterFiles: [],
+      fallback: [
+        {
+          source: '/:slug',
+          destination: `${blogUrl}/:slug`,
+        },
+      ],
+    };
   },
 };
  
